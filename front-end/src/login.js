75f4f7240794f865824d437d861e91e921c41c24
EL.MLIKI.HICHAM <92031524+El-Mliki-Hicham@users.noreply.github.com>
2022-10-05 08:34:07 -0700

    login

--- front-end/src/login.js
+++ front-end/src/login.js
@@ -3,32 +3,56 @@ import React from 'react';
 import { useState } from 'react';
 import axios from 'axios';
 import {useNavigate} from 'react-router-dom';
-
+import { useEffect } from 'react';
 const Login =()=>{
-const [password,SetPassword]=useState("");
-const [Email,SetEmail]=useState("");
+// const [password,SetPassword]=useState("");
 const navigate = useNavigate();
 
-async function log(){
-	
-	console.warn(Email,password)
+const [Data,SetData]=useState([]);
 
-	let items={Email}
+ useEffect(() =>{
 
-		let result = await fetch("http://127.0.0.1:8000/api/data", {
-		method: 'POST',
-		headers: {
-		"Content-Type": "application/json",
-		"Accept": "application/json"
-		},
-		body: JSON.stringify(items)
-		});
-		result = await result.json();
-		
-		navigate('/home')
-	 
+	// console.log(e.target.value)
+	let result =  axios.get("http://127.0.0.1:8000/api/data")
+
+	.then(res=>{
+
+		SetData(res.data)
+		console.log(res.data)
+	})
 
 }
+
+)
+
+// async function HandleChange(e){
+// 	let items = Email
+// 	console.log(e.target.value)
+// 	let result = await axios.get("http://127.0.0.1:8000/api/data")
+
+// 	.then(res=>{
+
+// 		SetEmail(res.data[0].email)
+// 		console.log(res.data[0].email)
+// 	})
+	
+    
+// }
+// async function log(){
+	
+// 		let result = await axios.get("http://127.0.0.1:8000/api/data")
+// 		.then(res=>{
+
+// 			console.log(res.data)
+// 		})
+	
+		
+		
+		
+// 		// navigate('/home')
+	 
+
+// }
 	
 	return(
 	<div className="limiter">
@@ -37,21 +61,23 @@ async function log(){
 				<div className="login100-pic js-tilt" data-tilt>
 					<img src="images/img-01.png" alt="IMG"></img>
 				</div>
-
+		{
+			Data.map(user=><h1>{user.id}</h1>)
+		}
 				<form className="login100-form validate-form">
 					<span className="login100-form-title">
 						 Login
 					</span>
 
 					<div className="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
-						<input className="input100" type="text" name="email" onChange={(e)=>SetEmail(e.target.value)} placeholder="Email"></input>
+						<input className="input100" type="text" name="email"  placeholder="Email"></input>
 						<span className="focus-input100"></span>
 						<span className="symbol-input100">
 							<i className="fa fa-envelope" aria-hidden="true"></i>
 						</span>
 					</div>
 
-					<div className="wrap-input100 validate-input"   onChange={(e)=>SetPassword(e.target.value)} data-validate = "Password is required">
+					<div className="wrap-input100 validate-input"   data-validate = "Password is required">
 						<input className="input100" type="password" name="pass" placeholder="Password"></input>
 						<span className="focus-input100"></span>
 						<span className="symbol-input100">
@@ -60,7 +86,7 @@ async function log(){
 					</div>
 					
 					<div className="container-login100-form-btn">
-						<button onClick={log} className="login100-form-btn">
+						<button  className="login100-form-btn">
 							Login
 						</button>
 					</div>
